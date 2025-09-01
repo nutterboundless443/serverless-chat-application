0b1b9211dbbf743901da6003524d87f4e7c78341
nutterboundless443 <serhiisiraj@hotmail.com>
2025-09-01 10:37:33 -0500

    Added validation for user input in sendMessage function

--- handler.js
+++ handler.js
@@ -13,7 +13,7 @@ module.exports.websocketDisconnect = async (event) => {
 
 module.exports.sendMessage = async (event) => {
   const message = JSON.parse(event.body);
-  if (!message.id || !message.content || !message.user) {
+  if (!message.id || !message.content || !message.user || typeof message.content !== 'string') {
     return {
       statusCode: 400,
       body: JSON.stringify({ message: 'Invalid message payload' }),
